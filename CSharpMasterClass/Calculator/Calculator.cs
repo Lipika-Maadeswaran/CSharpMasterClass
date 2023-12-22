c2f259cc44c74712710a40b6036a64c5e2fcfedd
Lipika Maadeswaran <lipika.maadeswaran@solitontech.com>
2023-12-22 23:33:43 +0530

    feat : Implemented a simple calculator functionality => Calculator.cs :- All calculation methods are added => Utilities.cs :- Methods to retrieve and display values are added

---
+++ CSharpMasterClass/Calculator/Calculator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    internal class Calculator
+    {
+        public string Addition(double input1, double input2) => 
+            $"The Addition of Numbers {input1} & {input2} gives => {input1 + input2}";
+
+        public string Subtraction(double input1, double input2) =>
+            $"The Subtraction of Numbers {input1} & {input2} gives => {input1 - input2}";
+
+        public string Multiplication(double input1, double input2) =>
+            $"The Multiplication of Numbers {input1} & {input2} gives => {input1 * input2}";
+
+        public string Division(double input1, double input2) =>
+            $"The Division of Numbers {input1} & {input2} gives => {input1 / input2}";
+
+    }
+}
---
+++ CSharpMasterClass/Calculator/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq.Expressions;
+using System.Text;
+
+namespace Calculator
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                Calculator calculator = new Calculator();
+                Utilities utilities = new Utilities(calculator);
+
+                Console.WriteLine("Welcome to World of Numbers!");
+                utilities.DisplayMessage();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+    }
+}
