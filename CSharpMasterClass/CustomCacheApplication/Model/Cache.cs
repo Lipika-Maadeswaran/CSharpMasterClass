d1db53ba0006899d0e082ef42c633fee9f0e8fb4
Lipika Maadeswaran <lipika.maadeswaran@solitontech.com>
2023-12-27 17:57:18 +0530

    feat:  Implement IDataDownloader interface and DataDownloader  in DataDownload folder to download data Create Cache class with caching mechanism in Model folder

---
+++ CSharpMasterClass/CustomCacheApplication/Model/Cache.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomCacheApplication.Model
+{
+    internal class Cache<TKey, TData>
+    {
+        private readonly Dictionary<TKey, TData> _cachedData = new();
+
+        public TData GetData(TKey key, Func<TKey, TData> value)
+        {
+            if (!_cachedData.ContainsKey(key))
+            {
+                _cachedData[key] = value(key);
+            }
+            return _cachedData[key];
+        }
+    }
+}
