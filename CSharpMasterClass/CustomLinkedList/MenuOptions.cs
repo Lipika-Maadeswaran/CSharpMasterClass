947d6a79c7e20f73df0c0ebfbd7f2928c962397b
Lipika Maadeswaran <lipika.maadeswaran@solitontech.com>
2024-01-03 13:10:44 +0530

    feat: Implemented custom linked list assignment: SinglyLinkedList class contains operations for 'contains', 'add', and 'remove'. Node class defines node details.

--- CSharpMasterClass/CustomLinkedList/MenuOptions.cs
+++ CSharpMasterClass/CustomLinkedList/MenuOptions.cs
@@ -8,13 +8,28 @@ namespace CustomLinkedList
 {
     public enum MenuOptions
     {
+        //Add items to list.
         Add = 1,
+
+        //Add items to front.
         AddToFront = 2,
-         AddToEnd = 3,
+
+        //Add items to end.
+        AddToEnd = 3,
+
+        //Checks element present.
         Contains = 4,
+
+        //Removes the element.
         Remove = 5,
+
+        //Clears the list.
         Clear = 6,
+
+        //Views all the item in list.
         View = 7,
+
+        //Exits the application.
         Exit = 8
     }
 }
