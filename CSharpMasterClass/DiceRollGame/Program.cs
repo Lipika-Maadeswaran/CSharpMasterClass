4578a0bcd78ea80aa9dde97db7d5883514a490b6
Lipika Maadeswaran <lipika.maadeswaran@solitontech.com>
2024-01-03 14:05:33 +0530

    feat : Implemented dice rolling game tests Game folder contains game files, UserCommunication folder has files that interact with user.

---
+++ CSharpMasterClass/DiceRollGame/Program.cs
@@ -0,0 +1,19 @@
+﻿using DiceRollGame.Game;
+using DiceRollGame.UserCommunication;
+
+namespace DiceRollGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var random = new Random();
+            var dice = new Dice(random);
+            var userCommunication = new ConsoleUserCommunication();
+            var guessingGame = new GuessingGame(dice, userCommunication);
+
+            GameResult gameResult = guessingGame.Play();
+            guessingGame.PrintResult(gameResult);
+        }
+    }
+}
